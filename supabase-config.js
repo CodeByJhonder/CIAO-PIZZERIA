9c8df2daf6216c0cdbe7f4da83faa9bb95c070a3
CodeByJhonder <jhondev20112025@gmail.com>
2026-08-01 20:45:01 -0400

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -4,7 +4,7 @@
 // pedidos) como en el panel de administración (para leerlos).
 // ============================================================
 
-const SUPABASE_URL = "https://iwmfmfdfchwmrlfquqg.supabase.co";
+const SUPABASE_URL = "https://iwmfmmfdfchwmrlfquqg.supabase.co";
 const SUPABASE_KEY = "sb_publishable_maWvUTY598tFmmWoHpU23g_LpYV8Dvl";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
